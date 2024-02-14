20c8248cdce7b03d3b93f8ad6f5b9f2deb613c6a
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2024-02-14 10:05:06 +0900

    admin admission test 3

--- resources/js/dashboard/admin-admission.js
+++ resources/js/dashboard/admin-admission.js
@@ -17,6 +17,8 @@ function qrCodeReading() {
         video.setAttribute('playsinline', true);
         video.play();
         video.addEventListener('canplay', startTick, false);
+        video.width = canvas.width;
+        video.height = canvas.height;
     })
         .catch((err) => {
             console.error(err);
@@ -26,8 +28,8 @@ function qrCodeReading() {
     function startTick() {
         msg.innerHTML = '読み取り中...';
         if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            // canvas.height = video.videoHeight;
-            // canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.width = video.videoWidth;
             ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
             let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
             let code = jsQR(imageData.data, imageData.width, imageData.height, {
