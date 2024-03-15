cc657da3de48520ff30956f1649101896b25130e
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2024-03-15 10:13:01 +0900

    company list tab-initialize fix 2

--- resources/views/main/list.blade.php
+++ resources/views/main/list.blade.php
@@ -1,6 +1,6 @@
 <x-template title="企業一覧" css="main.css" :overview="$overview" :isAdmission="$is_admission">
     <div class="hidden flex items-center justify-between w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow fixed bottom-20 left-4 z-50 toast inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 bg-blue-100 rounded-lg text-sm ms-3 font-normal ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 w-3 h-3"></div>
-    <div id="loading" class="bg-gray-700 bg-opacity-50 fixed z-[1000] w-full h-full top-0 bottom-0 left-0 right-0 flex justify-center items-center">
+    <div id="loading" class="bg-gray-700 bg-opacity-70 fixed z-[1000] w-full h-full top-0 bottom-0 left-0 right-0 flex justify-center items-center">
         <div class="flex flex-col items-center justify-center gap-4">
             <div class="load-wrapper">
                 <div class="load-circle"></div>
@@ -9,7 +9,14 @@
                 <div class="load-shadow"></div>
                 <div class="load-shadow"></div>
                 <div class="load-shadow"></div>
-                <span>Loading</span>
+            </div>
+            <div class="load-text text-center w-[200px]">
+                <p class="text-white text-center ml-3">Loading</p>
+                <div class="text-xs text-white mt-2">
+                    企業の画像の読み込みに
+                    <br>
+                    時間がかかる場合がございます。
+                </div>
             </div>
         </div>
     </div>
